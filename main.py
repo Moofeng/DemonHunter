3e4522e2701433de10e550cf986c4ae90828fc5f
Moofeng <36625561+Moofeng@users.noreply.github.com>
2019-08-05 10:28:33 +0800

    testing

--- main.py
+++
@@ -1,9 +0,0 @@
-import random
-import hashlib
-
-
-random_string = "".join([random.choice("abcdefghijk") for _ in range(5)])
-md5 = hashlib.md5()
-print(random_string)
-md5.update(f"{random_string}".encode("utf-8"))
-print(md5.hexdigest())
